7f7dd9883710d07a5ea3dcc5fc54e8bcbd1510a5
lambert <lambert.drn@gmail.com>
2022-10-27 18:05:51 +0200

    Correction petit bug avec le selector h1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar.js'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Accueil from './components/pages/Accueil'
-import Cv from './components/pages/Cv'
-import Experiences from './components/pages/Experiences'
-import Hobbies from './components/pages/Hobbies'
+import React from "react";
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Accueil from "./components/pages/Accueil";
+import Cv from "./components/pages/Cv";
+import Experiences from "./components/pages/Experiences";
+import Hobbies from "./components/pages/Hobbies";
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route exact path="/" element={<Accueil />} />
-                    <Route path="/cv" element={<Cv />} />
-                    <Route path="/experiences" element={<Experiences />} />
-                    <Route path="/hobbies" element={<Hobbies />} />
-                    <Route path="*" element={<Accueil />} />
-                </Routes>
-            </Router>
-        </>
-    )
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Accueil />} />
+          <Route path="/cv" element={<Cv />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/hobbies" element={<Hobbies />} />
+          <Route path="*" element={<Accueil />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
